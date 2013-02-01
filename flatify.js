2a6200da6c3da6f004ce24df42164e4109ed739a
DonPellegrino <hyssar@hotmail.com>
2013-02-01 12:29:49 -0500

    Initial commit

---
+++ flatify.js
@@ -0,0 +1,202 @@
+"v1.0.0";
+(function(){
+"use strict";
+
+	var _flatify = function(scope, options){
+		var self = this;
+		if (!scope){
+			if (typeof console === "object" && typeof console.log === "function"){
+				console.log("You must pass the 'this' object to the flatify constructor for all features to be available. Ex: new flatify(this);");
+			}
+			scope = {};
+		}
+		self.level = 0;
+		self._runCallbackCalled = false;
+		self.parentInstance = null;
+		//Default scope object
+		self._scope = {
+			"flatify" : {
+				"version" : "v1.0.0",
+				"masterInstance" : self,
+				"currentInstance" : self
+			}
+		};
+		//If the current instance is embedded, use its scope and set the references
+		if (scope.flatify && scope.flatify.version === self._scope.flatify.version){
+			self._scope = scope;
+			self.parentInstance = self._scope.flatify.currentInstance;
+			self._scope.flatify.currentInstance = self;
+			self.level = self.parentInstance.level + 1;
+		}
+		
+		options = options ? options : {};
+		self.defaultOptions = {
+			"cont" : (options.cont) ? options.cont : false,
+			"wait" : (options.wait || options.wait === false ) ? options.wait : true
+		};
+		//That way, user jobs can check the active flags
+		self.cont = self.defaultOptions.cont;
+		self.wait = self.defaultOptions.wait;
+		
+		self._stepsRan = 0;
+		self._steps = [];
+		self._callback = function(){};
+		
+		return self;
+	};
+	
+	_flatify.prototype.getIndex = function(){
+		var self = this;
+		return self._stepsRan;
+	}
+	
+	_flatify.prototype.getNumberJobs = function(){
+		var self = this;
+		return self._steps.length;
+	}
+	
+	_flatify.prototype.deleteJob = function(pos){
+		var self = this;
+		self._steps.splice(pos, 1);
+	}
+	
+	_flatify.prototype.seq = function(job, options, pos){
+		var self = this;
+		
+		if (job){
+			options = options ? options : {};
+			options = {
+				"cont" : (options.cont || options.cont === false) ? options.cont : self.defaultOptions.cont
+			};
+			var obj = {"job" : job, "par" : false, "options" : options};
+			if (pos || pos === 0){
+				self._steps.splice(pos, 0, obj);
+			}else{
+				self._steps.push(obj);
+			}
+		}
+		
+		return self;
+	};
+
+	_flatify.prototype.par = function(job, options){
+		var self = this;
+		if (job){
+			options = options ? options : {};
+			options = {
+				"cont" : (options.cont || options.cont === false) ? options.cont : self.defaultOptions.cont,
+				"wait" : (options.wait || options.wait === false) ? options.wait : self.defaultOptions.wait,
+				"nb"   : options.nb ? options.nb : 1
+			};
+			//Fill the jobs array
+			if (!(job instanceof Array)){
+				if (!(options.nb > 0)){
+					options.nb = 1;
+				}
+				var arr = [];
+				for (var i=0;i<options.nb;i++){
+					arr.push(job);
+				}
+				job = arr;
+			}
+			self._steps.push({"job" : job, "par" : true, "options" : options});
+		}
+		
+		return self;
+	};
+
+	_flatify.prototype.run = function(callback){
+		var self = this;
+		self._callback = callback;
+		if (self._steps.length === 0){
+			callback(null);
+		}else{
+			self._exec(0, null, []);
+		}
+		return self;
+	};
+
+	_flatify.prototype._exec = function(index, error, params){
+		var self = this;
+		//Move up one level if needed
+		if (self._scope.flatify.currentInstance._runCallbackCalled){
+			self._scope.flatify.currentInstance = self._scope.flatify.currentInstance.parentInstance;
+		}
+		//Call the right function on the job
+		if ((error && !self.cont) || self._stepsRan === self._steps.length){
+			self._execRun(error, params);
+		}else{
+			self.cont = self._steps[index].options.cont; //Set flag for current job
+			if (self._steps[index].par){
+				self.wait = self._steps[index].options.wait;
+				self._execPar(index, error, params, function(error, params){
+					self._stepsRan++;
+					self._exec(++index, error, params);
+				});
+			}else{
+				self._execSeq(index, error, params, function(error, params){
+					self._stepsRan++;
+					self._exec(++index, error, params);
+				});
+			}
+		}
+	};
+
+	_flatify.prototype._execRun = function(error, params){
+		var self = this;
+		self._runCallbackCalled = true;
+		params.unshift(error);
+		self._callback.apply(self._scope, params);
+	};
+
+	_flatify.prototype._execPar = function(index, error, params, callback){
+		var self = this;
+		
+		params.unshift(error);
+		var callbackIndex = params.length;
+		var outputs = [];
+		var errors = [];
+		var errorTrue = false; //At least one job had an error, so return the errors array instead of just null
+		var returned = 0;
+		var callbackCalled = false;
+		for (var i=0;i<self._steps[index].job.length;i++){
+			outputs[i] = [];
+			errors[i] = null;
+			params[callbackIndex] = (function(i){ //To keep the outputs array ordered the same way the user entered the jobs
+				return function(error){
+					outputs[i] = Array.prototype.slice.call(arguments, 1);
+					errors[i] = error;
+					if (error){
+						errorTrue = true;
+					}
+					returned++;
+					if ((returned === self._steps[index].job.length || (error && !self.wait)) && !callbackCalled){
+						callbackCalled = true;
+						if (!errorTrue){
+							errors = null;
+						}
+						callback(errors, [outputs]);
+					}
+				};
+			})(i);
+			self._steps[index].job[i].apply(self._scope, params);
+		}
+		
+	};
+	
+	_flatify.prototype._execSeq = function(index, error, params, callback){
+		var self = this;
+		
+		params.unshift(error);
+		params.push(function(error){
+			callback(error, Array.prototype.slice.call(arguments, 1));
+		});
+		self._steps[index].job.apply(self._scope, params);
+	};
+	
+	if (typeof module === "object" && typeof exports === "object"){//Node.js
+		exports.flatify = _flatify;
+	}else if (typeof window === "object" && window.window === window && typeof window.navigator === "object"){//Browser
+		window.flatify = _flatify;
+	}
+})();
---
+++ tests/testflatify.js
@@ -0,0 +1,190 @@
+var isNode = (typeof module === "object" && typeof exports === "object");
+var isBrowser = (typeof window === "object" && window.window === window && typeof window.navigator === "object");
+var consoleExists = (typeof console !== "undefined" && typeof console.log === "function");
+
+var output = function(str){
+	if (consoleExists){
+		console.log(str);
+	}
+	if (isBrowser){
+		document.write(str+"<br />");
+	};
+};
+var assert = {
+	strictEqual : function(param1, param2){
+		if (param1 !== param2){
+			var error = "ASSERT FAIL. "+param1+" !== "+param2;
+			output(error);
+			throw error;
+		}else{
+			output(param1+" === "+param2);
+		}
+	}
+};
+
+
+if (isNode){
+	var util = require("util");
+	global.con = function(variable){
+		util.puts(util.inspect(variable));
+	};
+	var flatify = require(__dirname + "/../flatify.js").flatify;
+
+}else if (isBrowser){
+	if (consoleExists){
+		window.con = function(obj){console.log(obj);};
+	}else{
+		window.con = function(obj){};
+	}
+}
+
+var timer = 1500;
+var t = new flatify(this, {"cont" : true}).seq(function(error, callback){
+	this.abc = "Shared context test";
+	this.def = 0;
+	new flatify(this).seq(function(error, callback){
+		assert.strictEqual(this.flatify.currentInstance.level, 1);
+		callback(null, "Sequential parameter passing test");
+	}).run(function(error, param){
+		assert.strictEqual(param, "Sequential parameter passing test");
+		
+		new flatify(this).par([function(error, callback){
+			assert.strictEqual(this.flatify.currentInstance.level, 2);
+			callback(null, "From #0a", "From #0b");
+		}, function(error, callback){
+			assert.strictEqual(this.abc, "Shared context test");
+			callback("Branch #1 error", "From #1");
+		}]).run(function(error, param){
+			assert.strictEqual(error[0], null);
+			assert.strictEqual(error[1], "Branch #1 error");
+			assert.strictEqual(param[0][0], "From #0a");
+			assert.strictEqual(param[0][1], "From #0b");
+			assert.strictEqual(param[1][0], "From #1");
+			callback(null, "Level ("+this.flatify.currentInstance.level+") up parameter passing test");
+		});
+	});
+}).par(function(error, param, callback){
+	var self = this;
+	setTimeout(function(){
+		assert.strictEqual(param, "Level (2) up parameter passing test");
+		assert.strictEqual(self.abc, "Shared context test");
+		assert.strictEqual(self.flatify.currentInstance.level, 1);
+		callback((self.def === 2 ? "TEST ERROR" : null), "From #"+(self.def++), self.abc);
+	}, (timer-=250));
+}, {"nb":4}).run(function(error, param){
+	assert.strictEqual(error[0], null);
+	assert.strictEqual(error[1], "TEST ERROR");
+	assert.strictEqual(error[2], null);
+	assert.strictEqual(error[3], null);
+	assert.strictEqual(param[0][0], "From #3");
+	assert.strictEqual(param[1][0], "From #2");
+	assert.strictEqual(param[2][0], "From #1");
+	assert.strictEqual(param[3][0], "From #0");
+});
+
+//Test empty queue
+var testRun = "";
+new flatify(this).run(function(error){
+	testRun += "!";
+});
+assert.strictEqual(testRun, "!");
+
+//Test wait
+var a = function(errors, param){
+	assert.strictEqual(errors[0], null);
+	assert.strictEqual(errors[1], "!!!");
+	assert.strictEqual(param[0].length, 0);
+	assert.strictEqual(param[1][0], "param 2");
+};
+var b = function(error, callback){
+	setTimeout(function(){
+		callback(null,"param 1");
+	}, 1500);
+};
+var d = function(error, callback){
+	callback(null);
+};
+var c = function(error, callback){
+	callback(null);
+};
+
+new flatify(this, {"wait":true}).par([b, function(error, callback){
+	new flatify(this).seq(d).seq(c).run(function(error){
+		callback("!!!", "param 2");
+	});
+}], {"wait":false}).run(a);
+
+// Test cont
+new flatify(this, {"cont":true}).seq(function(error, callback){
+	assert.strictEqual(error, null);
+	callback("!!!");
+}, {"cont":false}).seq(function(error, callback){
+	assert.strictEqual(true, false);
+	callback(null);
+}).run(function(error){
+	assert.strictEqual(error, "!!!");
+});
+
+//Test index
+new flatify(this, {"cont":true}).seq(function(error, callback){
+	this.flatify.currentInstance.seq(function(error, callback){
+		assert.strictEqual(error, "error 1");
+		callback("error 2");
+	}, null, this.flatify.currentInstance.getIndex()+1);
+	callback("error 1");
+}).seq(function(error, callback){
+	assert.strictEqual(error, "error 2");
+	callback("error 3");
+}).run(function(error){
+	assert.strictEqual(error, "error 3");
+});
+
+//Delete job
+new flatify(this, {"cont":true}).seq(function(error, callback){
+	this.flatify.currentInstance.deleteJob(this.flatify.currentInstance.getIndex()+1);
+	callback("error 1");
+}).seq(function(error, callback){
+	callback("error 2");
+}).seq(function(error, callback){
+	assert.strictEqual(error, "error 1");
+	callback("error 3");
+}).run(function(error){
+	assert.strictEqual(error, "error 3");
+});
+
+//DOCUMENTATION EXAMPLES
+new flatify(this).seq(function(error, callback){
+	this.test1 = "ABC";
+	this.test2 = [];
+	this.test3 = 1;
+	callback(null);
+}).par(function(error, callback){
+	this.test2.push(this.test3++);
+	callback(null);
+}, {"nb":5}).run(function(error){
+	assert.strictEqual(this.test1, "ABC");
+	assert.strictEqual(this.test2[0], 1);
+	assert.strictEqual(this.test2[1], 2);
+	assert.strictEqual(this.test2[2], 3);
+	assert.strictEqual(this.test2[3], 4);
+	assert.strictEqual(this.test2[4], 5);
+});
+
+new flatify(this).seq(function(error, callback){
+	this.test1 = "ABC";
+	new flatify(this).seq(function(error, callback){
+		assert.strictEqual(this.test1, "ABC");
+		this.test2 = "DEF";
+		callback(null);
+	}).run(function(error){
+		callback(null);
+	});
+}).run(function(error){
+	assert.strictEqual(this.test1, "ABC");
+	assert.strictEqual(this.test2, "DEF");
+});
+
+
+setTimeout(function(){
+	console.log("OK");
+}, 2200);
